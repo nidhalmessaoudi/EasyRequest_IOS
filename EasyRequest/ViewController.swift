d4522c5fddc3a990fbfde04bed73f7b7027908af
Nidhal Messaoudi <nidhalmessaoudi@hotmail.com>
2021-09-22 18:28:43 +0100

    Initial Commit

---
+++ EasyRequest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyRequest
+//
+//  Created by Nidhal Messaoudi on 9/22/21.
+//  Copyright © 2021 Nidhal Messaoudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
