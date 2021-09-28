aa4ec9893edd3468520f9e040cc7bc46d53199bc
Nidhal Messaoudi <nidhalmessaoudi@hotmail.com>
2021-09-28 11:42:26 +0100

    Url and TextField Validation

--- EasyRequest/ViewController.swift
+++ EasyRequest/ViewController.swift
@@ -11,9 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var requestMethodPicker: UIPickerView!
     @IBOutlet weak var requestUrlTextField: UITextField!
+    @IBOutlet weak var requestUrlView: UIView!
     
     let requestMethods = ["GET", "POST", "PATCH", "PUT", "DELETE"]
     var selectedMethod: String?
+    
+    var lastErrorLabel: UILabel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,35 +27,80 @@ class ViewController: UIViewController {
         requestUrlTextField.delegate = self
     }
     
+    func isValidUrl(url: String) -> Bool {
+        let urlRegEx = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$"
+        let urlTest = NSPredicate(format:"SELF MATCHES %@", urlRegEx)
+        let result = urlTest.evaluate(with: url)
+        return result
+    }
+    
     @IBAction func sendPressed(_ sender: UIButton) {
-        requestUrlTextField.endEditing(true)
-        let url = URL(string: requestUrlTextField.text!)!
-        var request = URLRequest(url: url)
-        request.httpMethod = selectedMethod!
-        
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            guard let data = data,
-                let response = response as? HTTPURLResponse,
-                error == nil else {
-                print("error", error ?? "Unknown error")
-                return
-            }
-
-            guard (200 ... 299) ~= response.statusCode else {
-                print("statusCode should be 2xx, but is \(response.statusCode)")
-                print("response = \(response)")
-                return
-            }
-
-            let responseString = String(data: data, encoding: .utf8)
-            print("responseString = \(responseString!)")
+        if requestUrlTextField.isFocused {
+            requestUrlTextField.endEditing(true)
         }
         
-        task.resume()
+        clearErrors()
+        
+        let requestUrlTextFieldValue = requestUrlTextField.text
+        
+        if requestUrlTextFieldValue == "" {
+                renderError(withValue: "Missing the request Url!")
+            return
+        }
+        
+        if isValidUrl(url: requestUrlTextFieldValue!) {
+            let url = URL(string: requestUrlTextFieldValue!)
+            var request = URLRequest(url: url!)
+            request.httpMethod = selectedMethod!
+            
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                guard let data = data,
+                    let response = response as? HTTPURLResponse,
+                    error == nil else {
+                        print("error", error ?? "Unknown error")
+                        return
+                }
+                
+                guard (200 ... 299) ~= response.statusCode else {
+                    print("statusCode should be 2xx, but is \(response.statusCode)")
+                    print("response = \(response)")
+                    return
+                }
+                
+                let responseString = String(data: data, encoding: .utf8)
+                print("responseString = \(responseString!)")
+            }
+            
+            task.resume()
+        } else {
+            renderError(withValue: "Invalid Url!")
+        }
     }
+    
+    func renderError(withValue value: String) {
+        let errorLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 92, height: 40))
+        errorLabel.text = value
+        errorLabel.textColor = .red
+        errorLabel.textAlignment = .center
+        errorLabel.font = UIFont.systemFont(ofSize: 18)
+        requestUrlView.addSubview(errorLabel)
+        
+        errorLabel.translatesAutoresizingMaskIntoConstraints = false
+        errorLabel.centerXAnchor.constraint(equalTo: requestUrlView.centerXAnchor).isActive = true
+        errorLabel.centerYAnchor.constraint(equalTo: requestUrlView.centerYAnchor, constant: -50).isActive = true
+        
+        lastErrorLabel = errorLabel
+    }
+    
+    func clearErrors() {
+        lastErrorLabel?.removeFromSuperview()
+        lastErrorLabel = nil
+    }
+        
 }
 
 extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
+    //MARK: - Picker View Extension
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -67,6 +115,12 @@ extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate, UITextFi
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         selectedMethod = requestMethods[row]
+    }
+    
+    //MARK: - Text Field Extension
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        clearErrors()
+        return true
     }
     
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
